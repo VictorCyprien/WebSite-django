a173c9cf30a40fbe69c8b3fe50b62de61c81b561
VictorCYPRIEN <>
2022-04-29 15:09:52 +0200

    Add `RegisterForm` for overload `UserCreationForm`

--- lycee/forms.py
+++ lycee/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
 from django.forms.models import ModelForm
 from .models import Student, Presence
 
@@ -43,3 +46,23 @@ class CursusCallForm(forms.Form):
     choices = forms.MultipleChoiceField(
         widget  = forms.CheckboxSelectMultiple,
     )
+
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(label = "Email")
+    first_name = forms.CharField(label = "First name")
+    last_name = forms.CharField(label = "Last name")
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email", )
+    
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
